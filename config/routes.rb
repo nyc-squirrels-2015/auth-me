c7fc23db1813c5afdb979299fcd6f1f5604b8e33
Steven Cassidy <steven@mumsnet.com>
2015-02-25 00:05:58 -0500

    Add friendlier login/out routes and login status in nav

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@ Rails.application.routes.draw do
   resources :users
   resources :sessions  
 
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
+
+
   resources :pages
   root 'users#new'
 
